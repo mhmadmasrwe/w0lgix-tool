84effb7b0073c372fb81579e30296c91cfdb270d
w0lgix <mhmad.msarwe@yahoo.com>
2016-08-18 04:18:25 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -9,22 +9,30 @@ $art =  "
 \033[1;36m#I███████████████████].                           #
 \033[1;33m#◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤...                               #
 \033[1;34m#                 coded by w0lgix                 #
-\033[1;36m#                 v2.0                            #
+\033[1;36m#                 v2.5                            #
 \033[1;32m#                                                 #
-\033[1;31m#      Using php admin.php www.example.com        #               
+\033[1;31m#  Using php admin.php www.example.com || list    #               
 \033[1;34m###################################################\n
 ";
 
 
-$site = $argv[1];
+$sites = $argv[1];
+if (is_file($sites)){
+$open = fopen($sites, 'r');
+$read = fread($open, filesize($sites));
+fclose($open);
+$sites = explode("\n", $read);
+echo $art;
+foreach ($sites as $site) 
+{
+   
 
 $arr = array('http://','https://','');
 $site1 = str_replace($arr, '', $site);
 
-echo $art;
 
 
-   $list = array ('/swadmin/index.php','a2c_cms/index.php','/admin/cms.php','/spip.php','/admin/admin.php','/web.config.txt','/cms','/phpmyadmin/index.php','robots.txt','/phpinfo.php','/404.php','admin/','administrator/','admin1/','admin2/','admin3/','admin4/','admin5/','usuarios/','usuario/','administrator/','moderator/','webadmin/','adminarea/','/
+   $list = array ('/Apanel/index.php','/apanel/index.php','/Administration/index.php','/swadmin/index.php','a2c_cms/index.php','/admin/cms.php','/spip.php','/admin/admin.php','/web.config.txt','/cms','/phpmyadmin/index.php','robots.txt','/phpinfo.php','/404.php','admin/','administrator/','admin1/','admin2/','admin3/','admin4/','admin5/','usuarios/','usuario/','administrator/','moderator/','webadmin/','adminarea/','/
 admin/','adminLogin/','admin_area/','panel-administracion/','instadmin/',
 'memberadmin/','administratorlogin/','adm/','admin/account.php','admin/index.php','admin/login.php','admin/admin.php','admin/account.php',
 'admin_area/admin.php','admin_area/login.php','siteadmin/login.php','siteadmin/index.php','siteadmin/login.html','admin/account.html','admin/index.html','admin/login.html','admin/admin.html',
@@ -87,16 +95,109 @@ $list1 = str_replace('/', '', $list);
     else { 
         // *nix 
         $cmd = system( 'clear' ); 
-        echo $art;
+        echo $ar;
     } 
 	
 }
 	
 		if(preg_match("/200/",$get[0]))
-		
+		  {
 			echo "\033[1;31m[+]\033[1;m"."\033[1;34mFound=>>\033[1;m"." \033[1;32mhttp://$site1/$path\033[1;m\n";
-		
+            $open1 = fopen("rezultsite.txt", "a");
+            $write = fwrite($open1, "http://$site1/$path\n");
+            fclose($open1);
+		  }
 		}
 		
-	
+	}
+ }
+
+else
+{
+
+$arr = array('http://','https://','');
+$site1 = str_replace($arr, '', $sites);
+
+echo $art;
+
+
+   $list = array ('/Apanel/index.php','/apanel/index.php','/Administration/index.php','/swadmin/index.php','a2c_cms/index.php','/admin/cms.php','/spip.php','/admin/admin.php','/web.config.txt','/cms','/phpmyadmin/index.php','robots.txt','/phpinfo.php','/404.php','admin/','administrator/','admin1/','admin2/','admin3/','admin4/','admin5/','usuarios/','usuario/','administrator/','moderator/','webadmin/','adminarea/','/
+admin/','adminLogin/','admin_area/','panel-administracion/','instadmin/',
+'memberadmin/','administratorlogin/','adm/','admin/account.php','admin/index.php','admin/login.php','admin/admin.php','admin/account.php',
+'admin_area/admin.php','admin_area/login.php','siteadmin/login.php','siteadmin/index.php','siteadmin/login.html','admin/account.html','admin/index.html','admin/login.html','admin/admin.html',
+'admin_area/index.php','bb-admin/index.php','bb-admin/login.php','bb-admin/admin.php','admin/home.php','admin_area/login.html','admin_area/index.html',
+'admin/controlpanel.php','admin.php','admincp/index.asp','admincp/login.asp','admincp/index.html','admin/account.html','adminpanel.html','webadmin.html',
+'webadmin/index.html','webadmin/admin.html','webadmin/login.html','admin/admin_login.html','admin_login.html','panel-administracion/login.html',
+'admin/cp.php','cp.php','administrator/index.php','administrator/login.php','nsw/admin/login.php','webadmin/login.php','admin/admin_login.php','admin_login.php',
+'administrator/account.php','administrator.php','admin_area/admin.html','pages/admin/admin-login.php','admin/admin-login.php','admin-login.php',
+'bb-admin/index.html','bb-admin/login.html','acceso.php','bb-admin/admin.html','admin/home.html','login.php','modelsearch/login.php','moderator.php','moderator/login.php',
+'moderator/admin.php','account.php','pages/admin/admin-login.html','admin/admin-login.html','admin-login.html','controlpanel.php','admincontrol.php',
+'admin/adminLogin.html','adminLogin.html','admin/adminLogin.html','home.html','rcjakar/admin/login.php','adminarea/index.html','adminarea/admin.html',
+'webadmin.php','webadmin/index.php','webadmin/admin.php','admin/controlpanel.html','admin.html','admin/cp.html','cp.html','adminpanel.php','moderator.html',
+'administrator/index.html','administrator/login.html','user.html','administrator/account.html','administrator.html','login.html','modelsearch/login.html',
+'moderator/login.html','adminarea/login.html','panel-administracion/index.html','panel-administracion/admin.html','modelsearch/index.html','modelsearch/admin.html',
+'admincontrol/login.html','adm/index.html','adm.html','moderator/admin.html','user.php','account.html','controlpanel.html','admincontrol.html',
+'panel-administracion/login.php','wp-login.php','adminLogin.php','admin/adminLogin.php','home.php','admin.php','adminarea/index.php',
+'adminarea/admin.php','adminarea/login.php','panel-administracion/index.php','panel-administracion/admin.php','modelsearch/index.php',
+'modelsearch/admin.php','admincontrol/login.php','adm/admloginuser.php','admloginuser.php','admin2.php','admin2/login.php','admin2/index.php','usuarios/login.php',
+'adm/index.php','adm.php','affiliate.php','adm_auth.php','memberadmin.php','administratorlogin.php','WSO.php','dz.php','a.php','wp-content/plugins/akismet/akismet.php','images/stories/w.php','w.php','shell.php','cpanel.php','cpn.php','sql.php','mysql.php','configuration.php','madspot.php','Cgishell.pl','killer.php','changeall.php','2.php','Sh3ll.php','dz0.php','dam.php','user.php','dom.php','whmcs.php','r00t.php','1.php','a.php','r0k.php','abc.php','egy.php','syrian_shell.php','xxx.php','settings.php','tmp.php','cyber.php','c99.php','r57.php','404.php','gaza.php','1.php','d4rk.php','index1.php','nkr.php','xd.php','M4r0c.php','Dz.php','sniper.php','ksa.php','v4team.php','offline.php','priv8.php','911.php','madspotshell.php','c100.php','sym.php','cp.php','tmp/cpn.php','tmp/w.php','tmp/r57.php','tmp/king.php','tmp/sok.php','tmp/ss.php','tmp/as.php','tmp/dz.php','tmp/r1z.php','tmp/whmcs.php','tmp/root.php','tmp/r00t.php','templates/beez/index.php','templates/beez/beez.php','templates/rhuk_milkyway/index.php','tmp/uploads.php','tmp/upload.php','tmp/sa.php','sa.php','readme.php','tmp/readme.php','wp.zip'.'wp-content/plugins/disqus-comment-system/disqus.php',
+'d0mains.php','wp-content/plugins/akismet/akismet.php','madspotshell.php','info.php','egyshell.php','Sym.php','c22.php','c100.php',
+'wp-content/plugins/akismet/admin.php#','g.php','wp-content/plugins/google-sitemap-generator/sitemap-core.php#',
+'wp-content/plugins/akismet/widget.php#','xx.pl','ls.php','Cpanel.php','k.php','zone-h.php','tmp/user.php','tmp/Sym.php','cp.php',
+'tmp/madspotshell.php','tmp/root.php','tmp/whmcs.php','tmp/index.php','tmp/2.php','tmp/dz.php','tmp/cpn.php',
+'tmp/changeall.php','tmp/Cgishell.pl','tmp/sql.php','0day.php','tmp/admin.php','cliente/downloads/h4xor.php',
+'whmcs/downloads/dz.php','L3b.php','d.php','tmp/d.php','tmp/L3b.php','wp-content/plugins/akismet/admin.php',
+'templates/rhuk_milkyway/index.php','templates/beez/index.php','sado.php','admin1.php','upload.php','up.php','vb.zip','vb.rar',
+'admin2.asp','uploads.php','sa.php','sysadmins/','admin1/','sniper.php','administration/Sym.php','images/Sym.php',
+'/r57.php','/wp-content/plugins/disqus-comment-system/disqus.php','gzaa_spysl','sql-new.php','/shell.php','/sa.php','/admin.php',
+'/sa2.php','/2.php','/gaza.php','/up.php','/upload.php','/uploads.php','/templates/beez/index.php','shell.php','/amad.php',
+'/t00.php','/dz.php','/site.rar','/Black.php','/site.tar.gz','/home.zip','/home.rar','/home.tar','/home.tar.gz',
+'/forum.zip','/forum.rar','/forum.tar','/forum.tar.gz','/test.txt','/ftp.txt','/user.txt','/site.txt','/error_log','/error',
+'/cpanel','/awstats','/site.sql','/vb.sql','/forum.sql','r00t-s3c.php','c.php','/backup.sql','/back.sql','/data.sql','wp.rar/',
+'wp-content/plugins/disqus-comment-system/disqus.php','asp.aspx','/templates/beez/index.php','tmp/vaga.php',
+'tmp/killer.php','whmcs.php','abuhlail.php','tmp/killer.php','tmp/domaine.pl','tmp/domaine.php','useradmin/',
+'tmp/d0maine.php','d0maine.php','tmp/sql.php','X.php','123.php','m.php','b.php','up.php','tmp/dz1.php','dz1.php','forum.zip','Symlink.php','Symlink.pl',
+'forum.rar','joomla.zip','joomla.rar','wp.php','buck.sql','sysadmin.php','images/c99.php', 'xd.php', 'c100.php',
+'spy.aspx','xd.php','tmp/xd.php','sym/root/home/','billing/killer.php','tmp/upload.php','tmp/admin.php',
+'Server.php','tmp/uploads.php','tmp/up.php','Server/','wp-admin/c99.php','tmp/priv8.php','priv8.php','cgi.pl/',
+'tmp/cgi.pl','downloads/dom.php','templates/ja-helio-farsi/index.php','webadmin.html','admins.php',"/wordpress-3.6-en.zip","/Joomla.zip","/joomla.zip","/backup1.zip"
+                ,"/wordpress-4.0-ar.zip","/wordpress-4.0.zip","/wordpress-3.6-en_CA.zip","/wordpress-3.6-ar.zip"
+                ,"/Joomla_3.3.6-Stable-Full_Package.zip","/Joomla_2.5.27-Stable-Full_Package.zip"
+                ,"/public_html.zip","/backup.zip","/vb.zip","/vb3.zip","/wcmadmin/index.php"
+                ,"/administrator.zip","/admin.zip","/new.zip","/up.zip","/upload.zip","/phpinfo.php","/info.php","/upload1.zip"
+                ,"/upload2.zip","/wordpress-3.8-tr_TR.zip","/wordpress-3.9.1-he_IL.zip","/wordpress-3.8.zip"
+                ,"/zoldick.php","/wp.zip","/includes/config.php~","/wordpress.zip","/admin","/login.php","/Log-in.html","/404.php"
+                ,"/r57.php","/shell.php","/sql","/pro.php","/upload.php","/priv8.php","/c99.php","/404.php","/wso.php","/admin.php","/DZ.php","/scama","/paypal"
+                ,"/5.php","/hacker","/dz.php","/login.php","/members.php","/backdoor.php","/angel.php","/123.php","/admin.aspx",'/login.php','purchasing_login.php','okbye.php','/phpMyAdmin','/adminG7sus4','/_admin','/admin1/index.php');
+
+$list1 = str_replace('/', '', $list);
+    foreach ($list1 as $path){
+        
+        $get = get_headers("http://".$site1."/".$path);
+
+        if ($site1 == null) {
+     if( stristr( php_uname( 's' ), 'Windows NT' ) ) { 
+        // Windows 
+        $cmd = system( 'cls' ); 
+        echo $art;
+    } 
+    else { 
+        // *nix 
+        $cmd = system( 'clear' ); 
+        echo $art;
+    } 
+    
+}
+    
+        if(preg_match("/200/",$get[0]))
+            {
+            echo "\033[1;31m[+]\033[1;m"."\033[1;34mFound=>>\033[1;m"." \033[1;32mhttp://$site1/$path\033[1;m\n";
+            $open1 = fopen("rezultsite.txt", "a");
+            $write = fwrite($open1, "http://$site1/$path\n");
+            fclose($open1);
+            }
+        }
+        
+    }
+
 ?>
